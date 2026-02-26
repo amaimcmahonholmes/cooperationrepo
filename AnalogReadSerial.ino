9da16120a5dc0aac9ecb54fc3f385296702471f1
econlon7 <s00289270@atu.ie>
2026-02-26 14:32:26 +0000

    inproved temp sensor

--- AnalogReadSerial.ino
+++ AnalogReadSerial.ino
@@ -9,10 +9,22 @@
 
   https://docs.arduino.cc/built-in-examples/basics/AnalogReadSerial/
 */
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
+      lcd.begin(16, 2);
+
+    lcd.setRGB(colorR, colorG, colorB);
+
   Serial.begin(9600);
 }
 
@@ -21,8 +33,15 @@ void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // print out the value you read:
-  Serial.println(sensorValue);
-  Serial.print("Temp: ");
+      lcd.setCursor(0, 1);
+      lcd.print("Temp: ");
+      lcd.print(sensorValue);
+   
+    // print the number of seconds since reset:
+    // lcd.print(millis() / 10);
+
+    delay(5);
+
   //lcd.print(sensorValue);
-  delay(1);  // delay in between reads for stability
+  delay(10);  // delay in between reads for stability
 }
