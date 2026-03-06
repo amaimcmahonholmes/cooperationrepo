13e33562616899d357ebda99e82cfe6d4c14ed5b
amaimcmahonholmes <S00286639@atu.ie>
2026-03-06 19:37:33 +0000

    Adding Comments to explain code

--- IncubatorCode/IncubatorCode.ino
+++ IncubatorCode/IncubatorCode.ino
@@ -1,20 +1,23 @@
 /*
   This example code is in the public domain.
+  We used the 'AnalogReadSerial' for some parts of the code as a small base.
 
-  modified by Aoife McMahon Holmes on 06/03/2026
+  Modified by Aoife McMahon Holmes on 06/03/2026
 */
-=======
 
 #include <Wire.h>
 #include "rgb_lcd.h"
 
+//We will add <Wifi.h> when we begin fully working on our internet component
 
+//getting lcd screen colour
 rgb_lcd lcd;
 
 const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
 
+//startTime is for the timer while resetTrigger is for if the button component is pushed or not
 unsigned long startTime = 0;
 bool resetTriggered = false;
 
@@ -25,28 +28,30 @@ void setup() {
 }
 
 void loop() {
-
+//the sensorValue is for the temperature sensor while the analogValue is for the 
   int sensorValue = analogRead(A0);
   int analogValue = analogRead(A1);
-  int voltRead = digitalRead(D2);
 
 //if button clicked
   if (analogValue >= 500 && !resetTriggered) {
     startTime = millis();
     resetTriggered = true;
   }
-
+//if button isn't clicked
   if (analogValue < 500) {
     resetTriggered = false;
   }
   
-//
+//elapsed is converting milliseconds into seconds for the timer
   unsigned long elapsed = (millis() - startTime) / 1000;
 
+//formating the LCD screen
   lcd.setCursor(0, 0);
   lcd.print("Time: ");
   lcd.print(elapsed);
   lcd.print("s     ");  
+
+//lcd.setCursor is used to have Temp display on a different line from the Time
 
   lcd.setCursor(0, 1);
   lcd.print("Temp: ");
