aacc77280111dd7fde3ead330b075e571d2d94a5
amaimcmahonholmes <S00286639@atu.ie>
2026-03-12 14:21:50 +0000

    Update IncubatorCode.ino

--- IncubatorCode/IncubatorCode.ino
+++ IncubatorCode/IncubatorCode.ino
@@ -7,12 +7,28 @@
 */
 
 #include <Wire.h>
+#include <math.h>
 #include "rgb_lcd.h"
 
+<<<<<<< Updated upstream:IncubatorCode/IncubatorCode.ino
 //We will add <Wifi.h> when we begin fully working on our internet component
 
 //getting lcd screen colour
+=======
+>>>>>>> Stashed changes:AnalogReadSerial/AnalogReadSerial.ino
 rgb_lcd lcd;
+
+#if defined(ARDUINO_ARCH_AVR)
+#define debug  Serial
+#elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
+#define debug  SerialUSB
+#else
+#define debug  Serial
+#endif
+
+const int B = 4275000;
+const int R0 = 100000;       
+
 
 const int colorR = 255;
 const int colorG = 0;
@@ -26,6 +42,7 @@ void setup() {
   lcd.begin(16, 2);
   lcd.setRGB(colorR, colorG, colorB);
   Serial.begin(9600);
+
 }
 
 void loop() {
@@ -42,11 +59,24 @@ void loop() {
   if (analogValue < 500) {
     resetTriggered = false;
   }
+<<<<<<< Updated upstream:IncubatorCode/IncubatorCode.ino
   
 //elapsed is converting milliseconds into seconds for the timer
   unsigned long elapsed = (millis() - startTime) / 1000;
 
 //formating the LCD screen
+=======
+
+  unsigned long elapsed = (millis() - startTime) / 1000;
+
+    int a = analogRead(sensorValue);
+
+    float R = 1023.0/a-1.0;
+    R = R0*R;
+
+    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;
+
+>>>>>>> Stashed changes:AnalogReadSerial/AnalogReadSerial.ino
   lcd.setCursor(0, 0);
   lcd.print("Time: ");
   lcd.print(elapsed);
@@ -56,8 +86,13 @@ void loop() {
 
   lcd.setCursor(0, 1);
   lcd.print("Temp: ");
-  lcd.print(sensorValue);
+  lcd.println(temperature);
   lcd.print("     ");
+
+  Serial.print("temperature = ");
+  Serial.println(temperature);
+  Serial.println(sensorValue);
+  Serial.println(R);
 
   delay(200);
 }
