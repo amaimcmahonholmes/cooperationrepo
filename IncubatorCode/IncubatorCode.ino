95b17cfded55763d3a2a5c211a3b1126b258bcf3
amaimcmahonholmes <S00286639@atu.ie>
2026-04-28 09:58:31 +0100

    Update IncubatorCode.ino

--- IncubatorCode/IncubatorCode.ino
+++ IncubatorCode/IncubatorCode.ino
@@ -1,133 +1,137 @@
 /*
-  This example code is in the public domain.
-  We used the 'AnalogReadSerial' for some parts of the code as a small base.
-
-  Modified by Eva conlon on 24/02/2026
-  Modified by Aoife McMahon Holmes on 06/03/2026
+  Fixed & Optimized Version
 */
+
+#define BLYNK_TEMPLATE_ID "TMPL4YeD_p0Ei"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "U3R_65SJoZQ7neM7CK83u3a-bz8IWT68"
+
+#define BLYNK_PRINT Serial
+
 #include <WiFiS3.h>
+#include <BlynkSimpleWiFiS3.h>
 #include <ArduinoJson.h>
-#include "arduino_secrets.h" 
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;     // the WiFi radio's status
+#include "arduino_secrets.h"
 
 #include <Wire.h>
 #include <math.h>
 #include "rgb_lcd.h"
 
-//We will add <Wifi.h> when we begin fully working on our internet component
+// WiFi
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
 
-//getting lcd screen colour
+// LCD
 rgb_lcd lcd;
-
-#if defined(ARDUINO_ARCH_AVR)
-#define debug  Serial
-#elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
-#define debug  SerialUSB
-#else
-#define debug  Serial
-#endif
-
-#define BLYNK_TEMPLATE_ID "TMPL4LGC99pRr"
-#define BLYNK_TEMPLATE_NAME "Quickstart Template"
-#define BLYNK_AUTH_TOKEN "5h_3HaU6yDrv6NxSBddFFKCM609TEWg5"
-
-const int B = 3975;
-const int R0 = 100000;       
-
-
 const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
 
-//startTime is for the timer while resetTrigger is for if the button component is pushed or not
+// Thermistor constants
+const int B = 3975;
+const int R0 = 100000;   // MUST match your hardware
+
+// Pins
+#define LED_PIN 3
+
+// Blynk
+WidgetLED led1(V1);
+BlynkTimer timer;
+
+// Timer + button logic
 unsigned long startTime = 0;
 bool resetTriggered = false;
 
+// LED state
+bool ledState = false;
 
-void setup() {
-  lcd.begin(16, 2);
-  lcd.setRGB(colorR, colorG, colorB);
-  Serial.begin(9600);
+/* ---------------- BLYNK ---------------- */
 
-  // // check for the WiFi module:
-  // if (WiFi.status() == WL_NO_MODULE) {
-  //   Serial.println("Communication with WiFi module failed!");
-  //   // don't continue
-  //   while (true);
-  // }
-  // // you're connected now, so print out the data:
-  // Serial.print("You're connected to the network");
-  // printCurrentNet();
-  // printWifiData();
-
-  pinMode(3, OUTPUT);
-
+BLYNK_WRITE(V0)
+{
+  int value = param.asInt();
+  digitalWrite(LED_PIN, value);
 }
 
-void loop() {
-//     // check the network connection once every 10 seconds:
-//  lcd.setCursor(0, 1);
-//  lcd.print(millis() / 1000);
+BLYNK_CONNECTED()
+{
+  Blynk.setProperty(V3, "url",
+    "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
 
-//   delay(10000);
-//   printCurrentNet();
-//   printWifiData();
+/* ---------------- FUNCTIONS ---------------- */
 
-//the sensorValue is for the temperature sensor while the analogValue is for the 
-  int sensorValue = analogRead(A0);
-  int analogValue = analogRead(A1);
+// Blink LED without blocking
+void blinkLED() {
+  ledState = !ledState;
+  digitalWrite(LED_PIN, ledState);
+}
 
-//if button clicked
+// Read sensors + update LCD + send to Blynk
+void readSensorAndDisplay() {
+
+  int sensorValue = analogRead(A0);  // temperature
+  int analogValue = analogRead(A1);  // button
+
+  // Button logic (start/reset timer)
   if (analogValue >= 500 && !resetTriggered) {
     startTime = millis();
     resetTriggered = true;
   }
-//if button isn't clicked
+
   if (analogValue < 500) {
     resetTriggered = false;
   }
-// <<<<<<< Updated upstream:IncubatorCode/IncubatorCode.ino
-  
-//elapsed is converting milliseconds into seconds for the timer
+
   unsigned long elapsed = (millis() - startTime) / 1000;
 
-//formating the LCD screen
-// =======
+  // Thermistor calculation (FIXED)
+  float resistance = (float)(1023 - sensorValue) * R0 / sensorValue;
+  float temperature = 1 / (log(resistance / R0) / B + 1 / 298.15) - 273.15;
 
-  // unsigned long elapsed = (millis() - startTime) / 1000;
-
-  float resistance = (float)(1023-sensorValue)*(10000/sensorValue);
-  float temperature = 1/(log(resistance/10000)/B+1/298.15)-273.15;
-
+  // LCD display
   lcd.setCursor(0, 0);
   lcd.print("Time: ");
   lcd.print(elapsed);
-  lcd.print("s     ");  
-
-//lcd.setCursor is used to have Temp display on a different line from the Time
+  lcd.print("s     ");
 
   lcd.setCursor(0, 1);
   lcd.print("Temp: ");
   lcd.print(temperature);
-  lcd.print(char(223));
-  lcd.write("C");
-  lcd.print("     ");
-  
+  lcd.print((char)223);
+  lcd.print("C     ");
 
-  Serial.print("temperature = ");
+  // Serial debug
+  Serial.print("Temp = ");
   Serial.println(temperature);
-  Serial.println(sensorValue);
-   Serial.println(resistance);
-  Serial.println(R0);
 
-  delay(200);
+  // Send to Blynk
+  Blynk.virtualWrite(V2, elapsed);
+  Blynk.virtualWrite(V4, sensorValue);
+  Blynk.virtualWrite(V5, temperature);
+}
 
-  digitalWrite(3, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(3, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+/* ---------------- SETUP ---------------- */
 
+void setup() {
+  Serial.begin(9600);
 
+  pinMode(LED_PIN, OUTPUT);
+
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+
+  // Connect to Blynk
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  // Timers
+  timer.setInterval(500L, readSensorAndDisplay); // sensor + LCD
+  timer.setInterval(1000L, blinkLED);            // LED blink
+}
+
+/* ---------------- LOOP ---------------- */
+
+void loop() {
+  Blynk.run();
+  timer.run();
 }
